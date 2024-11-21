ba80ff50269d59d1cfd9d37965a3b1d9b345f491
Glitch (carpal-dent-artichoke) <none>
2024-11-21 14:59:03 +0000

    ➰🌥️ Updated with Glitch

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
+document.addEventListener("DOMContentLoaded", function () {
+    // Liste des pages valides
+    const validPages = [
+        "/404",
+        "/",
+        "/catalog-eng",
+        "/catalog-es",
+        "/catalog-fr",
+        "/confirmation",
+        "/confirmation-eng",
+        "/confirmation-es",
+        "/eng",
+        "/es",
+        "/login",
+        "/login-eng",
+        "/login-es"
+    ];
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
+    // Récupération du chemin de l'URL actuelle
+    const currentPath = window.location.pathname;
+
+    // Si le chemin actuel n'est pas dans la liste des pages valides, redirige vers /404
+    if (!validPages.includes(currentPath)) {
+        window.location.href = "/404";
+    }
+});
+
+
+
+
+
 console.log("Hello 🌎");
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
+
 const btn = document.querySelector("button"); // Get the button from the page
 if (btn) { // Detect clicks on the button
   btn.onclick = function () {
@@ -19,16 +41,15 @@ if (btn) { // Detect clicks on the button
 }
 
 
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
 
-// Open file when the link in the preview is clicked
 let goto = (file, line) => {
   window.parent.postMessage(
     { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
   );
 };
-// Get the file opening button from its class name
 const filer = document.querySelectorAll(".fileopener");
 filer.forEach((f) => {
   f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
 });
+
+
